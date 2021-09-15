f84f7163a299b7b7079e4552800b52771ee4b40f
Erwin Henraat <e.henraat@ma-web.nl>
2021-09-15 10:54:16 +0200

    cascading classes

--- grid.js
+++ grid.js
@@ -3,81 +3,54 @@ let gridHolder = document.getElementById("grid");
 let selectorHolder = document.getElementById("selectorHolder");
 let tileSize = 25;
 let borderSize = 2;
-
 let activeSelector = 'default';
-
-
 button.addEventListener("click", ()=>{
-    
-   
     clearGrid();
-    buildGrid(getGridDimensions());
-
-    
+    buildGrid(getGridDimensions());    
 });
 addTileSelector('spawnpoint');  
 addTileSelector('path');      
 addTileSelector('waypoint');
 addTileSelector('buildable');
 addTileSelector('default');
-addTileSelector('deselect');
-
-if(selectorHolder.children.length > 0){
-    selectorHolder.addEventListener('click',(e)=>{
-        
-        if(e.target.id != "selectorHolder"){
-            activeSelector = e.target.id;
-            
-            //clear borders    
-            for(let i=0;i<selectorHolder.children.length;i++){
-                selectorHolder.children[i].style.borderWidth = "0px";
-            }
-            e.target.style.borderWidth = "2px";
-
-        }
-    });
-} 
-
 clickGridTiles();
+clickSelectors();
 
-
+function clickSelectors(){
+    if(selectorHolder.children.length > 0){
+        selectorHolder.addEventListener('click',(e)=>{
+            if(e.target.id != "selectorHolder"){
+                activeSelector = e.target.classList[1];                    
+                for(let i=0;i<selectorHolder.children.length;i++){
+                    selectorHolder.children[i].style.borderWidth = "0px";
+                }
+                e.target.style.borderWidth = "2px";
+            }
+        });
+    }
+}
 function addTileSelector(name){
     let size = 20;
     let margin = 10;
     let tileSelector = document.createElement("div");
-
-    tileSelector.id = name;
-    
-    tileSelector.className = "selector";
+    tileSelector.classList.add("selector");
+    tileSelector.classList.add(name);
     tileSelector.style.width = size + "px";
     tileSelector.style.height = size + "px";
-    
-//activate when using images    
-    let image = "url('" +name+ ".png')";
-   // tileSelector.style.backgroundImage = image;
-   
     tileSelector.style.left = selectorHolder.children.length * (size + margin) + margin +"px";
-    selectorHolder.appendChild(tileSelector);
-    
-   
-    
-
-    
+    selectorHolder.appendChild(tileSelector);   
 }
-function buildGrid(dimensions){
-        
+function buildGrid(dimensions){        
     let tileCount = dimensions.w * dimensions.h;
     for(let i = 0 ; i < tileCount ; i++)
     {
         let tile = document.createElement("div");
-        tile.className = "tile";
+        tile.classList.add("tile");       
         tile.style.width = tileSize+"px";
         tile.style.height = tileSize+"px";
         tile.style.borderWidth = borderSize+"px";        
         tile.style.left = (i % dimensions.w) * (tileSize + borderSize) + "px";
-        tile.style.top = Math.floor(i / dimensions.w) * (tileSize + borderSize)+ "px"; 
-        
-        
+        tile.style.top = Math.floor(i / dimensions.w) * (tileSize + borderSize)+ "px";               
         gridHolder.appendChild(tile);
     }
 }
@@ -88,30 +61,16 @@ function clearGrid(){
 }
 function getGridDimensions(){
     let width = document.getElementById("width").value;
-    let height = document.getElementById("height").value;
-    
+    let height = document.getElementById("height").value;    
     let dimensions = {"w":width,"h":height};
     return dimensions; 
 }   
-
-
-
 let colorDict = {spawnpoint:"red",path:"yellow", waypoint:"blue", buildable:"green", default:"grey"};
-
 function clickGridTiles(){
     gridHolder.addEventListener("click", (e) =>{
-
         if(e.target.getAttribute('id')!="grid"){
-            //e.target.style.backgroundImage = "url('test2.png')";
-            console.log(activeSelector);
-
             e.target.style.backgroundColor = colorDict[activeSelector];
-
-
-
-
-        }     
-        
+        }           
     });
 }
 
